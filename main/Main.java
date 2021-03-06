4da517f2cbe44e1d98cade4fb8293887d5dbdf0b
selfstudymaster <webselfmaster@gmail.com>
2021-03-06 18:05:43 +0900

    chap03 list03-03まで終了

--- main/Main.java
+++ main/Main.java
@@ -1,16 +1,31 @@
 package main;
 
 // chap03
-// chap03 list03-01
-public class Main {
+// list03-01
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		boolean weather = true;
+//		if (weather == true) {
+//			System.out.println("洗濯をします");
+//			System.out.println("散歩にいきます");
+//		} else {
+//			System.out.println("DVDを見ます");
+//		}
+//	}
+//}
 
-	public static void main(String[] args) {
-		boolean weather = true;
-		if (weather == true) {
-			System.out.println("洗濯をします");
-			System.out.println("散歩にいきます");
-		} else {
-			System.out.println("DVDを見ます");
-		}
-	}
-}
+//list03-02
+// 実行すると無限ループになるのでCtrl C で強制終了
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		boolean doorClose = true;
+//		while (doorClose == true) {
+//			System.out.println("ノックする");
+//			System.out.println("1分待つ");
+//		}
+//	}
+//}
+
+//list03-03は省略
