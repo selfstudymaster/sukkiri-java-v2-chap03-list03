355d55acfe69059ec4d02796c71d10616103677c
selfstudymaster <webselfmaster@gmail.com>
2021-03-06 17:50:44 +0900

    first commit

---
+++ main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+// chap03
+// chap03 list03-01
+public class Main {
+
+	public static void main(String[] args) {
+		boolean weather = true;
+		if (weather == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+	}
+}
